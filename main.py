69c9d38fab4cc2fac1919b9e6522adbab5f77b95
sarakeating <sjkeatingc@gmail.com>
2022-02-01 19:27:03 -0500

    Barebones of the challenge

---
+++ main.py
@@ -0,0 +1,17 @@
+## Program: GDSC February Challenge
+## Programmer: Sara Keating
+## Date of first version: 01/02/2022
+## Description: This program takes an input array and a target sum
+##				and returns a quadruple from the array that satisfies
+## 				the target sum.
+
+
+def findQuadruple(inputArray, targetSum):
+	
+	# Something
+
+	outputArray = []
+
+	return outputArray
+
+# Tests go here
