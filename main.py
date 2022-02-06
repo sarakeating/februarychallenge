25e6ec4abfc746a4a16be1939c310bde0b4abe1c
Sara Keating <sjkeatingc@gmail.com>
2022-02-05 19:16:28 -0500

    Added list variable for output

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-## Program: GDSC February Challenge
+## Filename: main.py
+##Program: GDSC February Challenge
 ## Programmer: Sara Keating
 ## Date of first version: 01/02/2022
 ## Description: This program takes an input array and a target sum
@@ -8,10 +9,12 @@
 
 def findQuadruple(inputArray, targetSum):
 	
-	# Something
-
 	outputArray = []
+
+	# calculations
 
 	return outputArray
 
+
 # Tests go here
+print(findQuadruple([7,6,4,-1,1,2], 16))
