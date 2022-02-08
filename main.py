5b82b09107553b3cdd083bcd31ced88ecec1ff76
Sara Keating <sjkeatingc@gmail.com>
2022-02-08 13:15:22 -0500

    Initial solution for 2 numbers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ## Filename: main.py
-##Program: GDSC February Challenge
+## Program: GDSC February Challenge
 ## Programmer: Sara Keating
 ## Date of first version: 01/02/2022
 ## Description: This program takes an input array and a target sum
@@ -12,9 +12,21 @@ def findQuadruple(inputArray, targetSum):
 	outputArray = []
 
 	# calculations
+	# suggestion: different loop for each of the 4 numbers
+
+	for x, item in enumerate(inputArray):
+
+		if item > targetSum:
+			continue 
+		
+		for i in range(x+1, len(inputArray)):
+			if item + inputArray[i] == targetSum:
+				outputArray.append([item, inputArray[i]])		
+	
 
 	return outputArray
 
 
 # Tests go here
-print(findQuadruple([7,6,4,-1,1,2], 16))
+# print(findQuadruple([7,6,4,-1,1,2], 16))
+print(findQuadruple([1, 2, 3, 4], 6))
